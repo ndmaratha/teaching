31fda71b5478d59e4180328b2352d2b5ba669d5f
Nayan Desai <ndmaratha01@gmail.com>
2023-01-27 13:30:23 +0530

    dom done

---
+++ dom.js
@@ -0,0 +1,16 @@
+const silligudi=document.querySelectorAll(".bca");
+//in query selector we use # for id
+//.dot for classname
+//tagname for  tag
+// silligudi.forEach(element => {
+
+//     element.innerHTML="nd";
+// });
+silligudi[2].innerHTML="hello";
+
+const abc=document.querySelector("#don");
+abc.innerHTML=`<p>
+<h1>lalu painter</h1>
+200 rupaye per hour
+</p>`;
+//`` backtick use for  add original html
