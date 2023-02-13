08ee1e5e5139310265e39d85e15bbaeae4a88c5e
Nayan Desai <ndmaratha01@gmail.com>
2023-02-13 19:23:56 +0530

    variable and object done

---
+++ javascript/lesson.js
@@ -0,0 +1,26 @@
+//Three data types "var","let","const"
+// var for older devices
+//we use let when we want to change the value later
+//we use const when we do not want to change the value
+
+//** OBJECT */
+//object is real world thing like car,student
+//Styntax  let/const object_name={code}
+//we access the values of object with .dot operator 
+// as shown in line no 22
+const developer={
+ name:"lalu painter", 
+ age:19,
+ class:{
+    fy:"shivam",
+    sy:"nd",
+ },
+ greeting(){
+    console.log("shadi me jarur aana")
+ },
+}
+console.log(developer.age);
+console.log(developer.name,"ki");
+console.log(developer.greeting());
+
+
